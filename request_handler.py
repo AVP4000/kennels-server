9eceb478cdbdbec50780218a7c8d7c7ada7b4d7e
Andre Phosarath <andrepho22@gmail.com>
2024-11-04 13:29:11 -0600

    Finished Book 2

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,12 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer, create_animal, create_location, create_employee, create_customer, delete_animal, delete_location, delete_employee, delete_customer, update_animal, update_location, update_employee, update_customer
+from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer
+from views import create_animal, create_location, create_employee, create_customer
+from views import delete_animal, delete_location, delete_employee, delete_customer
+from views import update_animal, update_location, update_employee, update_customer
+from views import get_customer_by_email, get_animals_by_location, get_employees_by_location, get_animals_by_status
+from urllib.parse import urlparse, parse_qs
 import json
+
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -11,27 +17,23 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It gives a description of the class or function
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
-    
+
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
-        path_params = path.split("/")
+        """Parse the url into the resource and id"""
+        parsed_url = urlparse(path)
+        path_params = parsed_url.path.split('/')  # ['', 'animals', 1]
         resource = path_params[1]
-        id = None
 
-        # Try to get the item at index 2
+        if parsed_url.query:
+            query = parse_qs(parsed_url.query)
+            return (resource, query)
+
+        pk = None
         try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
-
-        return (resource, id)  # This is a tuple
+            pk = int(path_params[2])
+        except (IndexError, ValueError):
+            pass
+        return (resource, pk)
 
     # Here's a class function
     def _set_headers(self, status):
@@ -53,52 +55,69 @@ class HandleRequests(BaseHTTPRequestHandler):
         """
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.send_header('Access-Control-Allow-Methods',
+                         'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                         'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
-        """Handles GET requests to the server
-        """
-        # Set the response code to 'Ok'
         self._set_headers(200)
-        response = {}  # Default response
 
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
+        response = {}
 
-        if resource == "animals":
-            if id is not None:
-                response = get_single_animal(id)
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
 
-            else:
-                response = get_all_animals()
-        
-        if resource == "locations":
-            if id is not None:
-                response = get_single_location(id)
+        # If the path does not include a query parameter, continue with the original if block
+        if '?' not in self.path:
+            (resource, id) = parsed
 
-            else:
-                response = get_all_locations()
-                
-        if resource == "employees":
-            if id is not None:
-                response = get_single_employee(id)
+            # It's an if..else statement
+            if resource == "animals":
+                if id is not None:
+                    response = get_single_animal(id)
 
-            else:
-                response = get_all_employees()
-                
-        if resource == "customers":
-            if id is not None:
-                response = get_single_customer(id)
+                else:
+                    response = get_all_animals()
 
-            else:
-                response = get_all_customers()
+            if resource == "locations":
+                if id is not None:
+                    response = get_single_location(id)
 
-        # This weird code sends a response back to the client
-        # json.dumps converts Python into JSON
+                else:
+                    response = get_all_locations()
+            if resource == "employees":
+                if id is not None:
+                    response = get_single_employee(id)
+
+                else:
+                    response = get_all_employees()
+            if resource == "customers":
+                if id is not None:
+                    response = get_single_customer(id)
+
+                else:
+                    response = get_all_customers()
+
+        else:  # There is a ? in the path, run the query param functions
+            (resource, query) = parsed
+
+            # see if the query dictionary has an email key
+            if query.get('email') and resource == 'customers':
+                response = get_customer_by_email(query['email'][0])
+            # Checks animals query for location_id key
+            if query.get('location_id') and resource == 'animals':
+                response = get_animals_by_location(query['location_id'][0])
+            # Checks employees query for location id
+            if query.get('location_id') and resource == 'employees':
+                response = get_employees_by_location(query['location_id'][0])
+            # Checks animals query for status
+            if query.get('status') and resource == 'animals':
+                response = get_animals_by_status(query['status'][0])
+
         self.wfile.write(json.dumps(response).encode())
 
     # Here's a method on the class that overrides the parent's method.
@@ -108,43 +127,65 @@ class HandleRequests(BaseHTTPRequestHandler):
         """
         # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
+
         # Convert JSON string to a Python dictionary
         post_body = json.loads(post_body)
 
         # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
-        # Initialize new animal/location/employee/customer
-        new_thing = None
+        # Initialize new animal
+        new_item = None
 
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
         # function next.
         if resource == "animals":
-            new_thing = create_animal(post_body)
+            new_item = create_animal(post_body)
 
-        if resource == "locations":
-            new_thing = create_location(post_body)
+        elif resource == "locations":
+            new_item = create_location(post_body)
 
-        if resource == "employees":
-            new_thing = create_employee(post_body)
+        elif resource == "employees":
+            new_item = create_employee(post_body)
 
-        if resource == "customers":
-            new_thing = create_customer(post_body)
+        elif resource == "customers":
+            new_item = create_customer(post_body)
 
         # Encode the new animal and send in response
-        self.wfile.write(json.dumps(new_thing).encode())
+        self.wfile.write(json.dumps(new_item).encode())
+
+    # Here's a method on the class that overrides the parent's method.
+    # It handles any DELETE request.
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        elif resource == "locations":
+            delete_location(id)
+
+        elif resource == "employees":
+            delete_employee(id)
+
+        elif resource == "customers":
+            delete_customer(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
 
     def do_PUT(self):
-        """Handles PUT requests to the server
-        """
-        self._set_headers(204)
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
         post_body = json.loads(post_body)
@@ -152,48 +193,32 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
-        # Delete a single animal from the list
+        # set default value of success
+        success = False
+
         if resource == "animals":
-            update_animal(id, post_body)
+            # will return either True or False from `update_animal`
+            success = update_animal(id, post_body)
+        elif resource == "locations":
+            success = update_location(id, post_body)
+        elif resource == "employees":
+            success = update_employee(id, post_body)
+        elif resource == "customers":
+            success = update_employee(id, post_body)
 
-        if resource == "locations":
-            update_location(id, post_body)
-
-        if resource == "employees":
-            update_employee(id, post_body)
-
-        if resource == "customers":
-            update_customer(id, post_body)
+        # handle the value of success
+        if success:
+            self._set_headers(204)
+        else:
+            self._set_headers(404)
 
         # Encode the new animal and send in response
         self.wfile.write("".encode())
 
-    def do_DELETE(self):
-      # Set a 204 response code
-      self._set_headers(204)
-
-      # Parse the URL
-      (resource, id) = self.parse_url(self.path)
-
-      # Delete a single animal from the list
-      if resource == "animals":
-          delete_animal(id)
-
-      if resource == "locations":
-          delete_location(id)
-
-      if resource == "employees":
-          delete_employee(id)
-
-      if resource == "customers":
-          delete_customer(id)
-
-      # Encode the new animal and send in response
-      self.wfile.write("".encode())
-
-
 # This function is not inside the class. It is the starting
 # point of this application.
+
+
 def main():
     """Starts the server on port 8088 using the HandleRequests class
     """
@@ -204,3 +229,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
--- views/employee_requests.py
+++ views/employee_requests.py
@@ -1,65 +1,169 @@
+import sqlite3
+import json
+from models import Employee, Location
+
 EMPLOYEES = [
-  {
+    {
         "id": 1,
-        "name": "Jenna Solis"
+        "name": "Jenna Solis",
     }
 ]
 
+
 def get_all_employees():
-    return EMPLOYEES
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.sqlite3") as conn:
 
-# Function with a single parameter
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT 
+            e.id,
+            e.name,
+            e.address,
+            e.location_id,
+            l.name location_name,
+            l.address location_address
+        FROM Employee e
+        JOIN Location l
+            ON l.id = e.location_id
+        """)
+
+        # Initialize an empty list to hold all employee representations
+        employees = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an employee instance from the current row.
+            employee = Employee(row['id'], row['name'],
+                                row['address'], row['location_id'])
+
+            # Create a Location instance from the current row
+            location = Location(
+                row['id'], row['location_name'], row['location_address'])
+
+            # Add the dictionary representation of the location to the employee
+            employee.location = location.__dict__
+
+            # see the notes below for an explanation on this line of code.
+            employees.append(employee.__dict__)
+
+    return employees
+
+
 def get_single_employee(id):
-    # Variable to hold the found employee, if it exists
-    requested_employee = None
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    # Iterate the EMPLOYEE list above. Very similar to the
-    # for..of loops you used in JavaScript.
-    for employee in EMPLOYEES:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if employee["id"] == id:
-            requested_employee = employee
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.address,
+            e.location_id
+        FROM employee e
+        WHERE e.id = ?
+        """, (id, ))
 
-    return requested_employee
+        # Load the single result into memory
+        data = db_cursor.fetchone()
 
+        # Create an employee instance from the current row
+        employee = Employee(data['id'], data['name'],
+                            data['address'], data['location_id'])
+
+        return employee.__dict__
+
+
+# POST
 def create_employee(employee):
-    # Get the id value of the last employee in the list
     max_id = EMPLOYEES[-1]["id"]
 
-    # Add 1 to whatever that number is
     new_id = max_id + 1
 
-    # Add an `id` property to the employee dictionary
     employee["id"] = new_id
 
-    # Add the employee dictionary to the list
     EMPLOYEES.append(employee)
 
-    # Return the dictionary with `id` property added
     return employee
 
+
+# DELETE
 def delete_employee(id):
-    # Initial -1 value for employee index, in case one isn't found
-    employee_index = -1
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        db_cursor = conn.cursor()
 
-    # Iterate the EMPLOYEES list, but use enumerate() so that you
-    # can access the index value of each item
-    for index, employee in enumerate(EMPLOYEES):
-        if employee["id"] == id:
-            # Found the employee. Store the current index.
-            employee_index = index
+        db_cursor.execute("""
+        DELETE FROM employee
+        WHERE id = ?
+        """, (id, ))
 
-    # If the employee was found, use pop(int) to remove it from list
-    if employee_index >= 0:
-        EMPLOYEES.pop(employee_index)
+# PUT
+
 
 def update_employee(id, new_employee):
-    # Iterate the EMPLOYEES list, but use enumerate() so that
-    # you can access the index value of each item.
-    for index, employee in enumerate(EMPLOYEES):
-        if employee["id"] == id:
-            # Found the employee. Update the value.
-            EMPLOYEES[index] = new_employee
-            break
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Employee
+            SET
+                name = ?,
+                address = ?,
+                location_id = ?
+        WHERE id = ?
+        """, (new_employee['name'], new_employee['address'],
+              new_employee['location_id'], id, ))
+
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
+
+    # return value of this function
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
+
+
+# GET Employees by Location
+
+def get_employees_by_location(location):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.address,
+            e.location_id
+        FROM Employee e
+        WHERE e.location_id = ?
+        """, (location, ))
+
+        employees = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            employee = Employee(
+                row['id'], row['name'], row['address'], row['location_id'])
+            employees.append(employee.__dict__)
+
+    return employees
+
         
